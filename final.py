15e0f1e7a9db2f944adfd6671a57eec3652b448d
mkg33 <maja.k.gwozdz@gmail.com>
2018-03-25 16:19:06 +0200

    menu

---
+++ final.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+#Copyright (c) 2017 Maja Gwozdz. Version 1.0. Latest release: 05-05-2017
+#Distributed under the MIT license.
+
+'''Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.'''
+
+
+try:
+
+    while True:
+        print('\n\n')
+        print('██████╗  ██████╗ ██╗  ██╗   ██╗███████╗███████╗███╗   ███╗     ██╗    ██████╗ ')
+        print('██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝██╔════╝██╔════╝████╗ ████║    ███║   ██╔═████╗')
+        print('██████╔╝██║   ██║██║   ╚████╔╝ ███████╗█████╗  ██╔████╔██║    ╚██║   ██║██╔██║')
+        print('██╔═══╝ ██║   ██║██║    ╚██╔╝  ╚════██║██╔══╝  ██║╚██╔╝██║     ██║   ████╔╝██║')
+        print('██╔═══╝ ██║   ██║██║    ╚██╔╝  ╚════██║██╔══╝  ██║╚██╔╝██║     ██║   ████╔╝██║')
+        print('██║     ╚██████╔╝███████╗██║   ███████║███████╗██║ ╚═╝ ██║     ██║██╗╚██████╔╝')
+        print('╚═╝      ╚═════╝ ╚══════╝╚═╝   ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝╚═╝ ╚═════╝ ')
+        print('')
+        print('******************************************************************************')
+        print('Full screen recommended. To zoom in: [CMD+]')
+        print('Copyright (c) 2017 Maja Gwozdz. Version: 1.0. Latest release: 08-06-2017.')
+        print('Distributed under the MIT license.')
+        print('******************************************************************************')
+
+    # ASCII signature generated here: http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+        print('\n WELCOME! What would you like to do? Press a number and [ENTER]:\n')
+        print('1. Record a polysemous string and let the software disambiguate it.')
+        start = input('')
+        if start == '1':
+            import disambiguate
+            break
+        else:
+            print(' ____ ____ ____ ____ ____ _________ ____ _________ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ ____ ')
+            print('||E |||N |||T |||E |||R |||       |||A |||       |||V |||A |||L |||I |||D |||       |||N |||U |||M |||B |||E |||R ||')
+            print('||__|||__|||__|||__|||__|||_______|||__|||_______|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__||')
+            print('|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|')
+            print('')
+except KeyboardInterrupt:
+    print('\nGoodbye!')
+
+
+
+
+
+                             
+                             
+
